a743b00214efad7aa5fb3d5e0fb4d723885e3d38
Memiez <iam_minny@hotmail.com>
2023-11-08 16:24:42 +0700

    First

--- my-ecommerce-dashboard/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-// src/store/index.ts
-import { createStore } from 'vuex';
-import dashboard from './modules/dashboard'; // ตัวอย่างโมดูล store สำหรับ dashboard
-
-export default createStore({
-  modules: {
-    dashboard
-  }
-});
